df51a2fd764f27555cdacab5e7b9e3d71156b240
Jonathan Gustafsson <jonathan.gustafsson@hotmail.com>
2019-09-25 19:55:52 +0200

    refresh pwa

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -19,6 +19,16 @@ const getPathName = location => {
   return location.pathname.replace('/en', '')
 }
 
+export const onServiceWorkerUpdateReady = () => {
+  if (
+    window.confirm(
+      'This site has been updated with new data. Do you wish to reload the site to get the new data?'
+    )
+  ) {
+    window.location.reload(true)
+  }
+}
+
 export const onPreRouteUpdate = ({
   location,
   prevLocation,
