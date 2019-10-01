62a2e884f718332a0d006518389381658ac69306
Jonathan Gustafsson <jonathan.gustafsson@hotmail.com>
2019-10-01 18:50:37 +0200

    Adding hard cache again

--- gatsby-config.js
+++ gatsby-config.js
@@ -122,31 +122,31 @@ module.exports = {
     },
     {
       resolve: `gatsby-plugin-offline`,
-      options: {
-        runtimeCaching: [
-          {
-            // Use cacheFirst since these don't need to be revalidated (same RegExp
-            // and same reason as above)
-            urlPattern: /(\.js$|\.css$|static\/)/,
-            handler: `NetworkFirst`,
-          },
-          {
-            // page-data.json files are not content hashed
-            urlPattern: /^https?:.*\page-data\/.*\/page-data\.json/,
-            handler: `NetworkFirst`,
-          },
-          {
-            // Add runtime caching of various other page resources
-            urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
-            handler: `StaleWhileRevalidate`,
-          },
-          {
-            // Google Fonts CSS (doesn't end in .css so we need to specify it)
-            urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
-            handler: `StaleWhileRevalidate`,
-          },
-        ],
-      },
+      // options: {
+      //   runtimeCaching: [
+      //     {
+      //       // Use cacheFirst since these don't need to be revalidated (same RegExp
+      //       // and same reason as above)
+      //       urlPattern: /(\.js$|\.css$|static\/)/,
+      //       handler: `NetworkFirst`,
+      //     },
+      //     {
+      //       // page-data.json files are not content hashed
+      //       urlPattern: /^https?:.*\page-data\/.*\/page-data\.json/,
+      //       handler: `NetworkFirst`,
+      //     },
+      //     {
+      //       // Add runtime caching of various other page resources
+      //       urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
+      //       handler: `StaleWhileRevalidate`,
+      //     },
+      //     {
+      //       // Google Fonts CSS (doesn't end in .css so we need to specify it)
+      //       urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
+      //       handler: `StaleWhileRevalidate`,
+      //     },
+      //   ],
+      // },
     },
   ],
 }
