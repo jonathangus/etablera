a23b79dbc174a7e5e0816de6a943e0b08eb82713
Jonathan Gustafsson <jonathan.gustafsson@hotmail.com>
2019-10-07 12:20:36 +0200

    wip

---
+++ src/pages/about.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import SEO from '../components/Seo'
+import AllCases from '../components/cases/all-cases/AllCases'
+import { useSetting } from '../contexts/SettingsContext'
+import { graphql } from 'gatsby'
+
+const AboutPage = ({ data }) => {
+  const t = useSetting()
+
+  return (
+    <React.Fragment>
+      <SEO
+        title={t('meta.about.title')}
+        description={t('meta.about.description')}
+      />
+      <AllCases cases={data.cases.nodes} />
+    </React.Fragment>
+  )
+}
+
+export const query = graphql`
+  query($locale: String) {
+    cases: allContentfulCase(
+      filter: { node_locale: { eq: $locale } }
+      sort: { fields: [weight], order: ASC }
+    ) {
+      ...caseNodes
+    }
+  }
+`
+
+export default AboutPage
